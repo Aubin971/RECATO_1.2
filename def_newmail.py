692b7794424749467cb9564ffa00354b00d84562
Aubin971 <66623206+Aubin971@users.noreply.github.com>
2024-03-28 15:05:04 -0400

    Update def_newmail.py

--- def_newmail.py
+++ def_newmail.py
@@ -2,10 +2,23 @@ from email.message import EmailMessage
 import ssl
 import smtplib
 from defin_recto2 import lire_csv_recto
+from info_cato import get_password
+import json
+from tkinter import messagebox
+
+info_config_id = "info.json"
+def load_info_config_id():
+    try:
+        with open(info_config_id, "r") as file:
+            data = json.load(file)
+            return data.get("info", "info")
+    except FileNotFoundError:
+        messagebox.showwarning("Input Error",'''You are not already connected to an account, do it directly on CATO''')
 
 Subject = 'Perte de votre mot de passe'
 pswrd = (lire_csv_recto("recto_mail_.csv"))[0]
-concombre = (lire_csv_recto("addresse_mailset.rct"))
+current_id = load_info_config_id()
+concombre = (get_password(current_id))
 
 def send_email(email_list):
     for person in email_list:
@@ -13,7 +26,7 @@ def send_email(email_list):
 
         body = f"""
         Voici votre mot de passe : 
-        {concombre[2]}
+        {concombre}
         """
 
         em = EmailMessage()
@@ -28,4 +41,4 @@ def send_email(email_list):
             smtp.login(email_sender,pswrd)
             smtp.sendmail(email_sender,person,em.as_string())
 
-send_email([concombre[1]])
+send_email([current_id])
